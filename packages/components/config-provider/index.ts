3f1c4777d821960b4ca59a330c6d789d7e8f88b0
zys8119 <770959294@qq.com>
2021-11-29 12:33:42 +0800

    [add] 打包测试

---
+++ packages/components/config-provider/index.ts
@@ -0,0 +1,5 @@
+import { withInstall } from '@element-plus/utils/with-install'
+import { ConfigProvider } from './src'
+
+export const ElConfigProvider = withInstall(ConfigProvider)
+export default ElConfigProvider
