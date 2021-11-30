dfe71d8b716c9a08f9dd7beeec1ce46c5656afd3
zys8119 <770959294@qq.com>
2021-11-30 10:47:20 +0800

    [add] wisdom-plus

--- packages/wisdom-plus/plugin.ts
+++ packages/wisdom-plus/plugin.ts
@@ -1,15 +1,3 @@
-import { ElInfiniteScroll } from '@wisdom-plus/components/infinite-scroll'
-import { ElLoading } from '@wisdom-plus/components/loading'
-import { ElMessage } from '@wisdom-plus/components/message'
-import { ElMessageBox } from '@wisdom-plus/components/message-box'
-import { ElNotification } from '@wisdom-plus/components/notification'
-import { ElPopoverDirective } from '@wisdom-plus/components/popover'
-
 export default [
-  ElInfiniteScroll,
-  ElLoading,
-  ElMessage,
-  ElMessageBox,
-  ElNotification,
-  ElPopoverDirective,
+
 ]
